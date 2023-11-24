a671fe8140652c416b23561bc96abd006a95deda
Abhishek Anil <141896050+Abhishekanil007@users.noreply.github.com>
2023-11-24 16:58:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const byteSize = (str) => {
   // write your code here
+	const blob = new Blob([str], { type: 'text/plain;charset=utf-8'});
+
+	return blob.size;
 };
 
-// Do not change the code below
+// Do not change the code below 
 const str = prompt("Enter some string.");
 alert(byteSize(str));
